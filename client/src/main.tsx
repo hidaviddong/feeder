6ae2ac63b0d230310497b45ef8258fe0da582646
Haoyu Dong <hidaviddong@gmail.com>
2024-09-26 20:37:04 +0800

    chore: Update ApolloClient URI to use VITE_BACKEND_URI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: import.meta.env.VITE_BACKEND_URI,
   cache: new InMemoryCache()
 })
 
