6f049cfa608d70bc3d5ae39836c870233b96ecd9
Haoyu Dong <hidaviddong@gmail.com>
2024-09-26 10:35:53 +0800

    chore: update layout

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,3 +1,18 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
 import { useQuery, gql } from '@apollo/client';
 
 
@@ -30,30 +45,48 @@ const GET_BLOGS_BY_SOURCE = gql`
 
 
 
-
 export default function Home() {
-  const { loading, error, data } = useQuery(GET_BLOGS_BY_SOURCE);
+  const { loading, error, data:blogs } = useQuery(GET_BLOGS_BY_SOURCE);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-  return data.blogsBySource.map(({source,blogs}:{source:string,blogs:Blogs}) => {
-        return (
-          <>
-        <h1 className='text-4xl text-red-500'>{source}</h1>
-          <div className='h-48 overflow-scroll border rounded-md w-3/4 '>   
-            <ul className='flex flex-col space-y-4'>
-              {blogs.map(({id,title,link,author,date,description}) => {
-                return (
-                  <li key={id} className='rounded-md border-2'>
-                    <a href={link}>{title}</a>
-                    <p>{description}</p>
-                    <p>{author}</p>
-                    <p>{date}</p>
+  const data = blogs.blogsBySource;
+  return (
+    <Tabs defaultValue={data[0]?.source} className="w-[800px]">
+      <TabsList className="grid w-full" style={{ gridTemplateColumns: `repeat(${data.length}, minmax(0, 1fr))` }}>
+        {data.map((source) => (
+          <TabsTrigger key={source.source} value={source.source}>
+            {source.source}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {data.map((source) => (
+        <TabsContent key={source.source} value={source.source}>
+          <Card>
+            <CardHeader>
+              <CardTitle>{source.source} Blogs</CardTitle>
+              <CardDescription>
+                Latest blog posts from {source.source}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="space-y-4 h-[400px] overflow-y-auto">
+                {source.blogs.map((blog) => (
+                  <li key={blog.id} className="p-4 rounded-md border">
+                    <a href={blog.link} className="text-lg font-semibold hover:underline">{blog.title}</a>
+                    <p className="text-sm text-gray-600 mt-1">{blog.description}</p>
+                    <div className="flex justify-between text-xs text-gray-500 mt-2">
+                      <span>{blog.author}</span>
+                      <span>{blog.date}</span>
+                    </div>
                   </li>
-                );
-              })}
-            </ul>
-          </div>
-          </>
-        );
-    })
+                ))}
+              </ul>
+            </CardContent>
+            <CardFooter>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      ))}
+    </Tabs>
+  )
 }
